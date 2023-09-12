6096205b78db160a97c86e6468291b4aae898d83
Tiskae <macbook@Tiskaes-MacBook-Pro.local>
2023-09-12 10:10:43 +0100

    feat: static file serving

--- main.go
+++ main.go
@@ -19,5 +19,9 @@ func main() {
 		fmt.Println("The M4 F82 is a German masterpiece from BMW. It boasts a powerful V8 engine producing an astonishing 512 HP stock. It ...")
 	})
 
+	// File serving
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.ListenAndServe(":80", nil)
 }
